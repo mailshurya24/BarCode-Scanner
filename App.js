2f65a2af97fa4c74163f28ae6bb62876443b408e
mailshurya24 <70964703+mailshurya24@users.noreply.github.com>
2021-02-18 21:36:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View , Image } from 'react-native';
+import Scanner from './screens/Scanner';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      
+        <Scanner/>
+      
+    );
+  }
+}
+
+
+
+
